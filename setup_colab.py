089009ea0ef2a6bb44228a9af7de9468d34093ae
Susanoo's <phamthanhtri.id2020@gmail.com>
2026-05-09 23:44:58 +0700

    Update format colabs

--- setup_colab.py
+++ setup_colab.py
@@ -1,12 +1,5 @@
 import os
 
-if not os.path.exists("/content/Machine_Learning"):
-    os.system(
-        "git clone https://github.com/Susantoco/Machine_Learning.git"
-    )
-
-os.chdir("/content/Machine_Learning")
-
 os.system("pip install -q -r requirements.txt")
 
 if not os.path.exists("features/raw"):
